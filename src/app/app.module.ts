3a5215ebf3219426e61ce9c34f39d24dd437514a
Vuthuyhangbamboo <hangvtph07414@fpt.edu.vn>
2020-04-18 20:16:01 +0700

    6 files modified, 22 added, 38 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,21 @@ import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { IntroduceComponent } from './introduce/introduce.component';
+import { ProductComponent } from './product/product.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
 
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContentComponent,
+    IntroduceComponent,
+    ProductComponent,
+    FooterComponent
   ],
   imports: [
     HttpClientModule,
