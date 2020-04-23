a9dc4e8d4355da1bc51e14ef58f894e57c8cc7df
Vuthuyhangbamboo <hangvtph07414@fpt.edu.vn>
2020-04-23 23:15:25 +0700

    4 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Product} from "../product";
 import { ProductService } from '../product.service';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { async } from '@angular/core/testing';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -11,12 +12,18 @@ export class CartComponent implements OnInit {
   products: Product[];
   page = 1;
   pageSize = 5;
+  costPrice = 0;
   constructor(
     private productService : ProductService
   ) { }
 
   ngOnInit() {
-    this.getProductOrder();
+    (async () => {
+      await new Promise((resolve, reject) => {
+        this.productService.getProductOrder().subscribe(data1 => {this.products=data1; resolve(data1)});
+      });
+      this.loadCostPrice();
+    })();
   }
 
   getProductOrder(){
@@ -28,6 +35,12 @@ export class CartComponent implements OnInit {
      this.products = this.products.filter(product => product.id != response.id);
    })
     // this.products = this.products.filter(product => product.id != id);
+  }
+
+  loadCostPrice() {
+    for(const data of this.products) {
+      this.costPrice = Number(this.costPrice) + Number(data.price);
+    }
   }
 
 }
--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,44 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ProductService } from '../product.service';
-import { Product } from '../product';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ProductService } from "../product.service";
+import { Product } from "../product";
 
 @Component({
-  selector: 'app-product-detail',
-  templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  selector: "app-product-detail",
+  templateUrl: "./product-detail.component.html",
+  styleUrls: ["./product-detail.component.css"]
 })
 export class ProductDetailComponent implements OnInit {
-  pro: Product = new Product();
+  // pro: Product = new Product();
   product: Product;
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private productService: ProductService
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.getProduct();
-    this.getProductt();
+    this.route.params.subscribe(id => {
+      const IdFace = id.productID.charAt(0) + id.productID.charAt(1);
+      if (IdFace == "je") {
+        this.getProductt();
+      } else {
+        this.getProduct();
+      }
+    });
   }
 
-
-  getProduct(){
+  getProduct() {
     this.route.params.subscribe(param => {
-        this.productService.showProductD(param.productID).subscribe(data => this.product = data);
-    })
+      this.productService
+        .showProductD(param.productID)
+        .subscribe(data => (this.product = data));
+    });
   }
 
-  getProductt(){
+  getProductt() {
     this.route.params.subscribe(param => {
-        this.productService.showProductJ(param.productID).subscribe(data => this.product = data);
-    })
+      this.productService
+        .showProductJ(param.productID)
+        .subscribe(data => (this.product = data));
+    });
   }
 
-  addProductOrder(){
-    this.productService.addProductOrder(this.pro).subscribe(data =>{
-      this.router.navigateByUrl("/cart")
-    } );
+  addProductOrder(id) {
+    // this.productService.addProductDe(this.pro).subscribe(data => {
+    //   this.router.navigateByUrl("/cart");
+    // });
+    this.productService.addProductOrder(this.product).subscribe(Response => {
+      this.router.navigateByUrl("/cart");
+    });
   }
-
 }
