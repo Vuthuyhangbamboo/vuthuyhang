61d80fc717d00167580225325242de0a7ff7f69c
Vuthuyhangbamboo <hangvtph07414@fpt.edu.vn>
2020-04-29 13:43:58 +0700

    7 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -24,6 +24,7 @@ export class CartComponent implements OnInit {
       await new Promise((resolve, reject) => {
         this.productService.getProductOrder().subscribe(data1 => {this.products=data1; resolve(data1)});
       });
+      
       this.loadCostPrice();
     })();
   }
@@ -36,16 +37,20 @@ export class CartComponent implements OnInit {
     (async () => {
       await new Promise((resolve, reject) => {
         this.productService.removeProductOrder(id).subscribe(response => {
-     this.products = this.products.filter(product => product.id != response.id);
-   })
-      });
-      this.loadCostPrice();
-    })();
+     this.products = this.products.filter(product => product.id != response.id);});  
+      this.productService.getProductOrder().subscribe(data => {this.products=data; resolve(data)});
+  })
+  this.router.navigateByUrl("/cart");
+  for(const data of this.products) {
+      this.costPrice = 0;
+      this.costPrice = Number(this.costPrice) + Number(data.price)*0.88;
+    }
+  })();
   }
 
   loadCostPrice() {
-    for(const data of this.products) {
-      this.costPrice = Number(this.costPrice) + Number(data.price);
+    for(const data1 of this.products) {
+      this.costPrice = Number(this.costPrice) + Number(data1.price)*0.88;
     }
   }
 
